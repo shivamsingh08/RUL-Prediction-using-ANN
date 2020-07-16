a0c3dddaf1207dc052639bffb669222d46a1a7b0
Shivam Singh <67107369+shivamsingh08@users.noreply.github.com>
2020-07-16 13:55:55 +0530

    Update and rename 17IM10021_OHM_Term project.py to RUL_prediction_Code.py

--- RUL_prediction_Code.py
+++ RUL_prediction_Code.py
@@ -1,28 +1,11 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[15]:
-
-
 
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-
-# In[16]:
-
-
 data=pd.read_csv("RUL_data.csv")
 
-
-# In[17]:
-
-
-#list(data.columns)
-
-
-# In[18]:
-
+list(data.columns)
 
 training=data.iloc[1:200,]
 testing=data.iloc[200:333,]
@@ -41,10 +24,6 @@ Y_train=training.iloc[:,28]
 
 Y_test=testing.iloc[:,28]
 
-
-# In[19]:
-
-
 params = {
     "epochs": [10, 20],
     "batch_size": [10,20,40],
@@ -59,29 +38,19 @@ params = {
 }
 
 
-# In[20]:
-
-
+#Using the NeuroEvaluation package to predict the best parameters for applyting GA to the model.
 from evolution import NeuroEvolution
-
-
-# In[21]:
 
 
 search = NeuroEvolution(generations = 10, population = 10, params=params)
 
 search.evolve(X_train_scaled, Y_train, X_test_scaled, Y_test)
 
-
-# In[22]:
-
+#Building the ANN model
 
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
-
-
-# In[23]:
 
 
 mod = Sequential()
@@ -89,8 +58,6 @@ mod.add(Dense(60, input_dim=27, activation='relu'))
 mod.add(Dense(60, activation='relu'))
 mod.add(Dense(60,  activation='relu'))
 
-
-# In[25]:
 
 
 mod.add(Dense(1, activation='relu'))
@@ -104,20 +71,15 @@ predicted=mod.predict(X_test_scaled)
 predicted=pd.Series(predicted.reshape(-1))
 
 
-# In[26]:
-
+#Saving the file
 
 pred.to_csv("RUL_pred_values.csv")
 
 
-# In[27]:
-
+#Evaluating the Model
 
 from sklearn.metrics import mean_squared_error
 from math import sqrt
-
-
-# In[28]:
 
 
 RMSE_h11 = sqrt(mean_squared_error(round(pred[18:23]),Y_test[18:23]))
@@ -126,15 +88,11 @@ RMSE_h13 = sqrt(mean_squared_error(round(pred[84:89]),Y_test[84:89]))
 RMSE_h14 = sqrt(mean_squared_error(round(pred[128:133]),Y_test[128:133]))
 
 
-# In[29]:
-
-
 import statistics
 print("Median RMSE:")
 statistics.median([RMSE_h11,RMSE_h12,RMSE_h13,RMSE_h14])
 
 
-# In[ ]:
 
 
 
